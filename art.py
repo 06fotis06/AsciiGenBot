43d4cb6efe8e5877281c5dfd48d3e346f736ad94
06fotis06 <83125818+06fotis06@users.noreply.github.com>
2023-07-01 05:04:50 +0300

    Update art.py

--- art.py
+++ art.py
@@ -15,25 +15,6 @@ class AsciiArt:
     def image(self):
             width, height = self.img.size
             aspect_ratio = height/width
-            new_width = 25
-            new_height = aspect_ratio * new_width * 0.55
-            img = self.img.resize((new_width, int(new_height)))
-            img = img.convert('L')
-            pixels = img.getdata()
-            chars = ["B", "S", "#", "&", "@", "$", "%", "⁂", "√", "⠅", "⠂"]
-            new_pixels = [chars[pixel//25] for pixel in pixels]
-            new_pixels = ''.join(new_pixels)
-            new_pixels_count = len(new_pixels)
-            ascii_image = [new_pixels[index:index + new_width]
-                  for index in range(0, new_pixels_count, new_width)]
-            ascii_image = "\n".join(ascii_image)
-            print(ascii_image)
-            file = "ascii_image.txt"
-            with open(file, "w") as f:
-            	f.write(ascii_image)
-            	print(colored(f"saved art image to file as {file}", "yellow"))
-            width, height = self.img.size
-            aspect_ratio = height/width
             new_width = 120
             new_height = aspect_ratio * new_width * 0.55
             img = self.img.resize((new_width, int(new_height)))
